c4e03b85b847e370421f31d422db70011dafa14b
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-13 22:01:46 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import s from './App.module.css';
 import {Header} from "./components/Header/Header";
 import {Sidebar} from "./components/Sidebar/Sidebar";
 import {Messages} from "./components/Messages/Messages";
+import {Home} from "./components/Home/Home";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Settings} from "./components/Settings/Settings";
+import {Friends} from "./components/Friends/Friends";
 
 const App = () => {
     return (
-        <div className={s.wrapper}>
-            <Header/>
-            <Sidebar/>
-            <div className={s.content}>
-                CONTENT
+        <BrowserRouter>
+            <div className={s.wrapper}>
+                <Header/>
+                <Sidebar/>
+                    <Routes>
+                        <Route path={"/home"} element={<Home/>}/>
+                        <Route path={"/messages"} element={<Messages/>}/>
+                        <Route path={"/friends"} element={<Friends/>}/>
+                        <Route path={"/settings"} element={<Settings/>}/>
+                    </Routes>
             </div>
-        </div>
+        </BrowserRouter>
     )
 }
 
