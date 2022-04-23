61b2194e167b01e70e6a9af8312e5bd286687837
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-04-23 19:09:27 +0300

    refactor: switch to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ReactDOM from "react-dom";
 import App from "./App";
 import {store} from "./store/store";
 import {Provider} from "react-redux";
 
 
-const mainDiv = document.getElementById('root')
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
     <Provider store={store}>
         <App/>
-    </Provider>, mainDiv
+    </Provider>
 );
-
 
 
 // If you want to start measuring performance in your app, pass a function
