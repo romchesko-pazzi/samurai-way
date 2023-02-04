b0eb07e26f83de371548f9f140e9de74db613b7d
romchesko-pazzi <romchesko.pazzi@gmail.com>
2023-02-04 13:44:18 +0300

    feat: add env file

--- src/data/instance.ts
+++ src/data/instance.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-import { apiKey } from './apiKey';
-
 export const instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0',
   headers: {
-    'api-key': apiKey,
+    'api-key': process.env.REACT_APP_API_KEY,
   },
 });
